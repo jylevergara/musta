e0b119da2549dbec90c9f57d0f9d40caeee5ebdb
Jyle Vergara <jylevergara@gmail.com>
2025-09-10 15:42:15 -0400

    fix issue where user cannot change the language

--- musta/Models/Language.swift
+++ musta/Models/Language.swift
@@ -9,7 +9,25 @@ struct Language: Codable, Identifiable {
 }
 
 struct Phrase: Codable, Identifiable {
-    let id = UUID()
+    let id: String
     let phrase: String
     let translation: String
+    
+    init(phrase: String, translation: String) {
+        self.id = UUID().uuidString
+        self.phrase = phrase
+        self.translation = translation
+    }
+    
+    // Custom decoder to handle the UUID generation
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.phrase = try container.decode(String.self, forKey: .phrase)
+        self.translation = try container.decode(String.self, forKey: .translation)
+        self.id = UUID().uuidString
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case phrase, translation
+    }
 }
